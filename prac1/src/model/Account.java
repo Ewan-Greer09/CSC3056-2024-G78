b63bcd401bfcab255c9464b946a808bb3d2eff01
Ewan Greer <egreer09@qub.ac.uk>
2024-02-06 20:03:33 +0000

    create models

---
+++ prac1/src/model/Account.java
@@ -0,0 +1,50 @@
+package src.model;
+
+import java.util.Date;
+
+public class Account {
+    String accountNumber;
+    String usernameOfAccountHolder;
+    String accountType;
+    Date accountOpeningDate;
+
+    public Account(String accountNumber, String usernameOfAccountHolder, String accountType, Date accountOpeningDate) {
+        this.accountNumber = accountNumber;
+        this.usernameOfAccountHolder = usernameOfAccountHolder;
+        this.accountType = accountType;
+        this.accountOpeningDate = accountOpeningDate;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getUsernameOfAccountHolder() {
+        return this.usernameOfAccountHolder;
+    }
+
+    public void setUsernameOfAccountHolder(String usernameOfAccountHolder) {
+        this.usernameOfAccountHolder = usernameOfAccountHolder;
+    }
+
+    public String getAccountType() {
+        return this.accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Date getAccountOpeningDate() {
+        return this.accountOpeningDate;
+    }
+
+    public void setAccountOpeningDate(Date accountOpeningDate) {
+        this.accountOpeningDate = accountOpeningDate;
+    }
+
+}
---
+++ prac1/src/model/User.java
@@ -0,0 +1,58 @@
+package src.model;
+
+public class User {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String mobileNumber;
+
+    public User(String username, String password, String firstName, String lastName, String mobileNumber) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return this.mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+}
